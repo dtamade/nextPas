d563b87f3e0e0a67fac2cc6e42ba5772929acebd
dtamade <dtamade@gmail.com>
2026-05-05 03:30:09 +0800

    refactor: extract JSON helper primitives from monolithic driver
    - Move JsonEscape, JsonString, BooleanText, AppendJsonField and variants
      to nextpas_json_helpers.pas
    - nextpas.pas uses the new unit instead of inline implementations
    - verify_local passes unchanged

--- tools/stage0/nextpas.pas
+++ tools/stage0/nextpas.pas
@@ -11,7 +11,7 @@ program nextpas;
 {$UNITPATH ../../compiler/targets}
 
 uses
-  SysUtils, process, target_config, nextpas_projection_types,
+  SysUtils, process, target_config, nextpas_json_helpers, nextpas_projection_types,
   np_compilation_session, np_target_facts,
   np_package_workflow, np_toolchain_profiles, np_toolchain_runner,
   np_workspace_model;
@@ -293,103 +293,6 @@ begin
     '  nextpas pkg inspect --workspace <root> --target linux-x86_64 ' +
     '[--toolchain-binding <id>]'
   );
-end;
-
-function JsonEscape(const Value: string): string;
-var
-  Index: SizeInt;
-begin
-  Result := '';
-  for Index := 1 to Length(Value) do
-    case Value[Index] of
-      '\':
-        Result := Result + '\\';
-      '"':
-        Result := Result + '\"';
-      #10:
-        Result := Result + '\n';
-      #13:
-        Result := Result + '\r';
-      #9:
-        Result := Result + '\t';
-    else
-      Result := Result + Value[Index];
-    end;
-end;
-
-function JsonString(const Value: string): string;
-begin
-  Result := '"' + JsonEscape(Value) + '"';
-end;
-
-function BooleanText(const Value: Boolean): string;
-begin
-  if Value then
-    Exit('true');
-
-  Result := 'false';
-end;
-
-procedure AppendJsonField(
-  var AFields: string;
-  const AName: string;
-  const AValue: string
-);
-begin
-  if AFields <> '' then
-    AFields := AFields + ',';
-  AFields := AFields + JsonString(AName) + ':' + AValue;
-end;
-
-procedure AppendJsonStringField(
-  var AFields: string;
-  const AName: string;
-  const AValue: string
-);
-begin
-  if AValue = '' then
-    Exit;
-
-  AppendJsonField(AFields, AName, JsonString(AValue));
-end;
-
-procedure AppendJsonStringFieldWhenEnabled(
-  var AFields: string;
-  const AName: string;
-  const AValue: string;
-  const AEnabled: Boolean
-);
-begin
-  if not AEnabled then
-    Exit;
-
-  AppendJsonField(AFields, AName, JsonString(AValue));
-end;
-
-procedure AppendJsonIntegerField(
-  var AFields: string;
-  const AName: string;
-  const AValue: LongInt;
-  const AEnabled: Boolean
-);
-begin
-  if not AEnabled then
-    Exit;
-
-  AppendJsonField(AFields, AName, IntToStr(AValue));
-end;
-
-procedure AppendJsonBooleanField(
-  var AFields: string;
-  const AName: string;
-  const AValue: Boolean;
-  const AEnabled: Boolean
-);
-begin
-  if not AEnabled then
-    Exit;
-
-  AppendJsonField(AFields, AName, BooleanText(AValue));
 end;
 
 procedure AppendBuildContextProjectionJsonFields(var AFields: string);
