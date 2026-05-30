646af64025a6bb0946d8d5c32bbcc8e6f3c574ed
dtamade <dtamade@gmail.com>
2026-05-30 23:42:13 +0800

    test(llvm): add interface dispatch smoke test (exit=99 via IMT thunk)

---
+++ examples/smoke/llvm_interface_dispatch.pas
@@ -0,0 +1,20 @@
+program llvm_interface_dispatch;
+type
+  ICounter = interface
+    function Count: Integer;
+  end;
+  TImpl = class(TObject, ICounter)
+    function Count: Integer;
+  end;
+
+function TImpl.Count: Integer;
+begin
+  Count := 99;
+end;
+
+var
+  C: ICounter;
+begin
+  C := TImpl.Create;
+  Halt(C.Count);
+end.
