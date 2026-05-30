52a13126dbd6314feed96fc58547e1cfb78ca097
dtamade <dtamade@gmail.com>
2026-05-30 23:58:04 +0800

    test(llvm): add interface 'is' check smoke test (compile-time fold)

---
+++ examples/smoke/llvm_interface_is.pas
@@ -0,0 +1,24 @@
+program llvm_interface_is;
+type
+  ICounter = interface
+    function Count: Integer;
+  end;
+  TImpl = class(TObject, ICounter)
+    function Count: Integer;
+  end;
+
+function TImpl.Count: Integer;
+begin
+  Count := 1;
+end;
+
+var
+  O: TImpl;
+  R: Integer;
+begin
+  O := TImpl.Create;
+  R := 0;
+  if O is ICounter then
+    R := 50;
+  Halt(R);
+end.
