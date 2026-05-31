26ff4c7fa8e1b3b8885c74a4af7230d7e94e3ac5
dtamade <dtamade@gmail.com>
2026-05-31 13:19:38 +0800

    test(smoke): add Observer pattern + memoized Fibonacci class tests
    - llvm_observer_pattern: polymorphic event dispatch through base class
      parameter, stateful counter observer (exit=48)
    - llvm_fibonacci_class: memoized Fib with field array + recursive self
      method call + conditional array read/write (exit=55)
    
    84/84 LLVM smoke tests pass

---
+++ examples/smoke/llvm_fibonacci_class.pas
@@ -0,0 +1,34 @@
+program llvm_fibonacci_class;
+type
+  TFibCalc = class
+    FMemo: array of Integer;
+    FSize: Integer;
+    constructor Create(N: Integer);
+    function Fib(N: Integer): Integer; virtual;
+  end;
+
+constructor TFibCalc.Create(N: Integer);
+begin
+  FSize := N + 1;
+  SetLength(FMemo, FSize);
+  FMemo[0] := 0;
+  FMemo[1] := 1;
+end;
+
+function TFibCalc.Fib(N: Integer): Integer;
+begin
+  if N <= 1 then
+    Result := N
+  else
+  begin
+    if FMemo[N] = 0 then
+      FMemo[N] := Fib(N - 1) + Fib(N - 2);
+    Result := FMemo[N];
+  end;
+end;
+
+var C: TFibCalc;
+begin
+  C := TFibCalc.Create(10);
+  Halt(C.Fib(10));
+end.
---
+++ examples/smoke/llvm_observer_pattern.pas
@@ -0,0 +1,64 @@
+program llvm_observer_pattern;
+type
+  TObserver = class
+    FId: Integer;
+    constructor Create(Id: Integer);
+    function OnEvent(Data: Integer): Integer; virtual;
+  end;
+  TLogger = class(TObserver)
+    constructor Create(Id: Integer);
+    function OnEvent(Data: Integer): Integer; override;
+  end;
+  TCounter = class(TObserver)
+    FCount: Integer;
+    constructor Create(Id: Integer);
+    function OnEvent(Data: Integer): Integer; override;
+  end;
+
+constructor TObserver.Create(Id: Integer);
+begin
+  FId := Id;
+end;
+function TObserver.OnEvent(Data: Integer): Integer;
+begin
+  Result := 0;
+end;
+
+constructor TLogger.Create(Id: Integer);
+begin
+  FId := Id;
+end;
+function TLogger.OnEvent(Data: Integer): Integer;
+begin
+  Result := Data;
+end;
+
+constructor TCounter.Create(Id: Integer);
+begin
+  FId := Id;
+  FCount := 0;
+end;
+function TCounter.OnEvent(Data: Integer): Integer;
+begin
+  FCount := FCount + 1;
+  Result := FCount;
+end;
+
+function Notify(O: TObserver; Data: Integer): Integer;
+begin
+  Result := O.OnEvent(Data);
+end;
+
+var
+  L: TLogger;
+  C: TCounter;
+  R: Integer;
+begin
+  L := TLogger.Create(1);
+  C := TCounter.Create(2);
+  R := Notify(L, 42);
+  R := R + Notify(C, 0);
+  R := R + Notify(C, 0);
+  R := R + Notify(C, 0);
+  Halt(R);
+end.
