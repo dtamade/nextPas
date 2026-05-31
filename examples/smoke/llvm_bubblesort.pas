d47c4c981d9850115a581ace6b275c08fcc96ec6
dtamade <dtamade@gmail.com>
2026-05-31 13:43:52 +0800

    test(smoke): add bubble sort test (nested for + array swap + Arr[J+1] expr)
    Classic bubble sort on integer array. Tests nested for loops, array
    element comparison (Arr[J] > Arr[J+1]), conditional swap with temp
    variable, and computed array index expressions. (exit=53)
    
    88/88 LLVM smoke tests pass

---
+++ examples/smoke/llvm_bubblesort.pas
@@ -0,0 +1,24 @@
+program llvm_bubblesort;
+var
+  Arr: array of Integer;
+  I, J, Tmp, N: Integer;
+begin
+  N := 5;
+  SetLength(Arr, N);
+  Arr[0] := 42;
+  Arr[1] := 17;
+  Arr[2] := 93;
+  Arr[3] := 5;
+  Arr[4] := 28;
+
+  for I := 0 to N - 2 do
+    for J := 0 to N - 2 - I do
+      if Arr[J] > Arr[J + 1] then
+      begin
+        Tmp := Arr[J];
+        Arr[J] := Arr[J + 1];
+        Arr[J + 1] := Tmp;
+      end;
+
+  Halt(Arr[0] * 10 + Arr[4] mod 10);
+end.
