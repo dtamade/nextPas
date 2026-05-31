ad201c399ecbce05babb45e47ad4852eab5e2d63
dtamade <dtamade@gmail.com>
2026-05-31 11:30:42 +0800

    test(smoke): add Strategy + Visitor pattern tests (abstract, override, multi-arg vcall)
    - llvm_strategy_pattern: abstract Compare + override + multi-arg vcall
      through base class parameter (exit=40)
    - llvm_visitor_pattern: polymorphic Accept with args through base class
      function parameter (exit=26)
    
    73/73 LLVM smoke tests pass

---
+++ examples/smoke/llvm_strategy_pattern.pas
@@ -0,0 +1,52 @@
+program llvm_strategy_pattern;
+type
+  TSorter = class
+    constructor Create;
+    function Compare(A, B: Integer): Integer; virtual; abstract;
+  end;
+  TAscending = class(TSorter)
+    constructor Create;
+    function Compare(A, B: Integer): Integer; override;
+  end;
+  TDescending = class(TSorter)
+    constructor Create;
+    function Compare(A, B: Integer): Integer; override;
+  end;
+
+constructor TSorter.Create; begin end;
+constructor TAscending.Create; begin end;
+constructor TDescending.Create; begin end;
+
+function TAscending.Compare(A, B: Integer): Integer;
+begin
+  if A < B then
+    Result := 1
+  else
+    Result := 0;
+end;
+
+function TDescending.Compare(A, B: Integer): Integer;
+begin
+  if A > B then
+    Result := 1
+  else
+    Result := 0;
+end;
+
+function FindMin(S: TSorter; A, B, C: Integer): Integer;
+begin
+  Result := A;
+  if S.Compare(B, Result) = 1 then
+    Result := B;
+  if S.Compare(C, Result) = 1 then
+    Result := C;
+end;
+
+var
+  Asc: TAscending;
+  Desc: TDescending;
+begin
+  Asc := TAscending.Create;
+  Desc := TDescending.Create;
+  Halt(FindMin(Asc, 30, 10, 20) + FindMin(Desc, 30, 10, 20));
+end.
---
+++ examples/smoke/llvm_visitor_pattern.pas
@@ -0,0 +1,56 @@
+program llvm_visitor_pattern;
+type
+  TElement = class
+    FVal: Integer;
+    constructor Create(V: Integer);
+    function Accept(Multiplier: Integer): Integer; virtual;
+  end;
+  TDoubler = class(TElement)
+    constructor Create(V: Integer);
+    function Accept(Multiplier: Integer): Integer; override;
+  end;
+  TTripler = class(TElement)
+    constructor Create(V: Integer);
+    function Accept(Multiplier: Integer): Integer; override;
+  end;
+
+constructor TElement.Create(V: Integer);
+begin
+  FVal := V;
+end;
+function TElement.Accept(Multiplier: Integer): Integer;
+begin
+  Result := FVal * Multiplier;
+end;
+constructor TDoubler.Create(V: Integer);
+begin
+  FVal := V;
+end;
+function TDoubler.Accept(Multiplier: Integer): Integer;
+begin
+  Result := FVal * Multiplier * 2;
+end;
+constructor TTripler.Create(V: Integer);
+begin
+  FVal := V;
+end;
+function TTripler.Accept(Multiplier: Integer): Integer;
+begin
+  Result := FVal * Multiplier * 3;
+end;
+
+function Visit(E: TElement; M: Integer): Integer;
+begin
+  Result := E.Accept(M);
+end;
+
+var
+  D: TDoubler;
+  T: TTripler;
+  E: TElement;
+begin
+  D := TDoubler.Create(5);
+  T := TTripler.Create(3);
+  E := TElement.Create(7);
+  Halt(Visit(D, 1) + Visit(T, 1) + Visit(E, 1));
+end.
