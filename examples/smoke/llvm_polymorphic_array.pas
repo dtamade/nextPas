eabb99255f5c9f7272eb096effe32ec4df885ac3
dtamade <dtamade@gmail.com>
2026-05-31 13:42:32 +0800

    test(smoke): add polymorphic array test (Arr[I].Area with mixed subclasses)
    TCircle and TSquare stored in array of TShape, iterated with for loop
    calling virtual Area method. Tests array-of-objects + polymorphic vcall
    dispatch through base class array element. (exit=64)
    
    87/87 LLVM smoke tests pass

---
+++ examples/smoke/llvm_polymorphic_array.pas
@@ -0,0 +1,39 @@
+program llvm_polymorphic_array;
+type
+  TShape = class
+    constructor Create;
+    function Area: Integer; virtual;
+  end;
+  TCircle = class(TShape)
+    FR: Integer;
+    constructor Create(R: Integer);
+    function Area: Integer; override;
+  end;
+  TSquare = class(TShape)
+    FS: Integer;
+    constructor Create(S: Integer);
+    function Area: Integer; override;
+  end;
+
+constructor TShape.Create; begin end;
+function TShape.Area: Integer; begin Result := 0; end;
+
+constructor TCircle.Create(R: Integer); begin FR := R; end;
+function TCircle.Area: Integer; begin Result := FR * FR * 3; end;
+
+constructor TSquare.Create(S: Integer); begin FS := S; end;
+function TSquare.Area: Integer; begin Result := FS * FS; end;
+
+var
+  Arr: array of TShape;
+  I, S: Integer;
+begin
+  SetLength(Arr, 3);
+  Arr[0] := TCircle.Create(2);
+  Arr[1] := TSquare.Create(5);
+  Arr[2] := TCircle.Create(3);
+  S := 0;
+  for I := 0 to 2 do
+    S := S + Arr[I].Area;
+  Halt(S);
+end.
