4f8ab938b99a880e701ee43c8dbc9c1855cf3cce
dtamade <dtamade@gmail.com>
2026-06-01 03:44:06 +0800

    test(smoke): string function param + concat + return (Greet('World') → 'Hi World', exit=8)
    135/135 LLVM smoke tests pass

---
+++ examples/smoke/llvm_string_func.pas
@@ -0,0 +1,13 @@
+program test_str_concat_func;
+
+function Greet(Name: String): String;
+begin
+  Result := 'Hi ' + Name;
+end;
+
+var S: String;
+begin
+  S := Greet('World');
+  WriteLn(S);
+  Halt(Length(S));
+end.
