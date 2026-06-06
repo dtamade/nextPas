e32feeec2267dd96ccd6f4d0c096644387b6414f
dtamade <dtamade@gmail.com>
2026-06-06 11:07:10 +0800

    fix(lockfree): reject overflowing power-of-two capacity

--- core/src/nextpas.core.lockfree.base.pas
+++ core/src/nextpas.core.lockfree.base.pas
@@ -13,6 +13,17 @@ function LockFreeIsPow2(const AValue: PtrUInt): Boolean;
 
 implementation
 
+uses
+  nextpas.core.errors;
+
+function LockFreeMaxPowerOfTwo: PtrUInt; inline;
+var
+  LMax: PtrUInt;
+begin
+  LMax := not PtrUInt(0);
+  Result := LMax - (LMax shr 1);
+end;
+
 function LockFreeIsPow2(const AValue: PtrUInt): Boolean;
 begin
   Result := (AValue > 0) and ((AValue and (AValue - 1)) = 0);
@@ -22,6 +33,8 @@ function LockFreeNextPow2(const AValue: PtrUInt): PtrUInt;
 begin
   if LockFreeIsPow2(AValue) then
     Exit(AValue);
+  if AValue > LockFreeMaxPowerOfTwo then
+    raise EArgumentError.Create('LockFreeNextPow2: capacity exceeds maximum power-of-two');
   Result := 1;
   while Result < AValue do
     Result := Result shl 1;
