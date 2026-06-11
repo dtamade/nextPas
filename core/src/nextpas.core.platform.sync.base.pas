007f810ba6ba7b22de2b1fd49e8f2293c85c60bd
dtamade <dtamade@gmail.com>
2026-06-12 03:45:15 +0800

    core: import posix sync base directly

--- core/src/nextpas.core.platform.sync.base.pas
+++ core/src/nextpas.core.platform.sync.base.pas
@@ -6,27 +6,32 @@ interface
 
 {$IFDEF NEXTPAS_LINUX}
 uses
-  nextpas.core.platform.linux.base;
+  nextpas.core.platform.linux.base,
+  nextpas.core.platform.posix.base;
 {$ENDIF}
 
 {$IFDEF NEXTPAS_MACOS}
 uses
-  nextpas.core.platform.darwin.base;
+  nextpas.core.platform.darwin.base,
+  nextpas.core.platform.posix.base;
 {$ENDIF}
 
 {$IFDEF NEXTPAS_ANDROID}
 uses
-  nextpas.core.platform.android.base;
+  nextpas.core.platform.android.base,
+  nextpas.core.platform.posix.base;
 {$ENDIF}
 
 {$IFDEF NEXTPAS_FREEBSD}
 uses
-  nextpas.core.platform.freebsd.base;
+  nextpas.core.platform.freebsd.base,
+  nextpas.core.platform.posix.base;
 {$ENDIF}
 
 {$IF defined(NEXTPAS_UNIX) and not defined(NEXTPAS_LINUX) and not defined(NEXTPAS_MACOS) and not defined(NEXTPAS_ANDROID) and not defined(NEXTPAS_FREEBSD)}
 uses
-  nextpas.core.platform.unix.base;
+  nextpas.core.platform.unix.base,
+  nextpas.core.platform.posix.base;
 {$ENDIF}
 
 {$IFDEF NEXTPAS_WINDOWS}
