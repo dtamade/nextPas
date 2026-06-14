433294a01d60cef2a4953e2694699cc2f18e0787
dtamade <dtamade@gmail.com>
2026-06-14 18:40:58 +0800

    feat(tls): migrate 2 more files from TStringList to TStringArray
    - debug.utils: FBuffer field TStringList→TStringArray, .Text→StringsJoin
    - openssl.api.engine: function return type TStringList→TStringArray

--- core/src/nextpas.core.tls.debug.utils.pas
+++ core/src/nextpas.core.tls.debug.utils.pas
@@ -116,7 +116,7 @@ type
    *}
   TSSLStringBuilder = class
   private
-    FBuffer: TStringList;
+    FBuffer: TStringArray;
     FIndentLevel: Integer;
     FIndentStr: string;
   public
@@ -551,23 +551,23 @@ end;
 constructor TSSLStringBuilder.Create;
 begin
   inherited;
-  FBuffer := TStringList.Create;
+  FBuffer := nil;
   FIndentLevel := 0;
   FIndentStr := '  ';
 end;
 
 destructor TSSLStringBuilder.Destroy;
 begin
-  FBuffer.Free;
+  
   inherited;
 end;
 
 procedure TSSLStringBuilder.Append(const AStr: string);
 begin
-  if FBuffer.Count = 0 then
+  if Length(FBuffer) = 0 then
     FBuffer.Add('');
 
-  FBuffer[FBuffer.Count - 1] := FBuffer[FBuffer.Count - 1] + AStr;
+  FBuffer[Length(FBuffer) - 1] := FBuffer[Length(FBuffer) - 1] + AStr;
 end;
 
 procedure TSSLStringBuilder.AppendLine(const AStr: string);
@@ -611,7 +611,7 @@ end;
 
 function TSSLStringBuilder.ToString: string;
 begin
-  Result := FBuffer.Text;
+  Result := StringsJoin(FBuffer, sLineBreak);
 end;
 
 { TSSLBitSet }
