d91b3db02ef359be7a90163408546f4587a5f2f6
dtamade <dtamade@gmail.com>
2026-06-29 04:12:23 +0800

    chore: add .gitignore for Rust target/ and Cargo.lock

--- bench/arena/target/release/deps/bench_arena-8b62ecebe9d4138b.d
+++
@@ -1,5 +0,0 @@
-/home/dtamade/projects/nextPas/.worktrees/bench/bench/arena/target/release/deps/bench_arena-8b62ecebe9d4138b.d: benches/bench_arena.rs
-
-/home/dtamade/projects/nextPas/.worktrees/bench/bench/arena/target/release/deps/bench_arena-8b62ecebe9d4138b: benches/bench_arena.rs
-
-benches/bench_arena.rs:
--- bench/arena/target/release/deps/ciborium_io-19abb13333f2d4de.d
+++
@@ -1,7 +0,0 @@
-/home/dtamade/projects/nextPas/.worktrees/bench/bench/arena/target/release/deps/ciborium_io-19abb13333f2d4de.d: /home/dtamade/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ciborium-io-0.2.2/src/lib.rs
-
-/home/dtamade/projects/nextPas/.worktrees/bench/bench/arena/target/release/deps/libciborium_io-19abb13333f2d4de.rlib: /home/dtamade/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ciborium-io-0.2.2/src/lib.rs
-
-/home/dtamade/projects/nextPas/.worktrees/bench/bench/arena/target/release/deps/libciborium_io-19abb13333f2d4de.rmeta: /home/dtamade/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ciborium-io-0.2.2/src/lib.rs
-
-/home/dtamade/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ciborium-io-0.2.2/src/lib.rs:
