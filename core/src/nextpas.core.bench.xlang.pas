f2ac5443542399fb62d5adeff0d86a47bcdce448
dtamade <dtamade@gmail.com>
2026-07-08 10:14:21 +0800

    refactor(bench): xlang ParseLines 通用解析函数
    - 提取 ParseLines 通用解析框架
      * TXLangLineParser 单行解析函数类型
      * TXLangLineMatcher 行匹配函数类型
      * 一次排序多次查询，减少重复代码
    
    - ParseGoBenchOutput/ParseRustBenchOutput/ParseFPCBenchOutput 改用 ParseLines
      * 每个函数从 ~30 行降至 ~3 行
      * 消除重复的 SplitLines/IsEmptyOrComment/try-except 模式
    
    - 测试: 23 suites / 430 tests / 0 leaks

--- core/src/nextpas.core.bench.xlang.pas
+++ core/src/nextpas.core.bench.xlang.pas
@@ -24,6 +24,12 @@ type
    *}
   TXLangParser = (xlGo, xlRust, xlFPC);
 
+  {** 单行解析函数类型 }
+  TXLangLineParser = function(const ALine: string): TBenchResult;
+
+  {** 行匹配函数类型 }
+  TXLangLineMatcher = function(const ALine: string): Boolean;
+
   {** 解析错误（re-export 自 nextpas.core.exception） }
   EParseError = nextpas.core.exception.EParseError;
 
@@ -67,6 +73,15 @@ type
 
   {** 获取上次解析时跳过的行数 }
   function GetLastParseSkippedCount: Integer;
+
+  {** 通用行解析函数
+   *  @param AOutput 原始输出文本
+   *  @param ALineParser 单行解析函数
+   *  @param ALineMatcher 行匹配函数（返回 True 表示该行需要解析）
+   *  @return 解析结果数组 }
+  function ParseLines(const AOutput: string;
+    ALineParser: TXLangLineParser;
+    ALineMatcher: TXLangLineMatcher): TBenchResultArray;
 
 implementation
 
@@ -129,6 +144,43 @@ begin
   if LTrimmed = '' then
     Exit(True);
   Result := LTrimmed[1] = '#';
+end;
+
+function ParseLines(const AOutput: string;
+  ALineParser: TXLangLineParser;
+  ALineMatcher: TXLangLineMatcher): TBenchResultArray;
+var
+  LLines: TStringArray;
+  LLine: string;
+  LList: array of TBenchResult;
+  LCount: Integer;
+  LCapacity: Integer;
+begin
+  GLastParseSkippedCount := 0;
+  LList := nil;
+  LCount := 0;
+  LCapacity := 0;
+  LLines := SplitLines(AOutput);
+  for LLine in LLines do
+  begin
+    if IsEmptyOrComment(LLine) then Continue;
+    if not ALineMatcher(LLine) then Continue;
+
+    try
+      if LCount >= LCapacity then
+      begin
+        if LCapacity = 0 then LCapacity := 8
+        else LCapacity := LCapacity * 2;
+        SetLength(LList, LCapacity);
+      end;
+      LList[LCount] := ALineParser(LLine);
+      Inc(LCount);
+    except
+      Inc(GLastParseSkippedCount);
+    end;
+  end;
+  SetLength(LList, LCount);
+  Result := LList;
 end;
 
 { Helper: Safely compute TotalNs = NsPerOp * Iterations
@@ -307,39 +359,26 @@ end;
 
 { ParseGoBenchOutput }
 
-function ParseGoBenchOutput(const AOutput: string): TBenchResultArray;
-var
-  LLines: TStringArray;
-  LLine: string;
-  LList: array of TBenchResult;
-  LCount: Integer;
-  LCapacity: Integer;
-begin
-  GLastParseSkippedCount := 0;
-  LList := nil;
-  LCount := 0;
-  LCapacity := 0;
-  LLines := SplitLines(AOutput);
-  for LLine in LLines do
-  begin
-    if IsEmptyOrComment(LLine) then Continue;
-    if Pos('Benchmark', LLine) <> 1 then Continue;
+{ 行匹配器 }
 
-    try
-      if LCount >= LCapacity then
-      begin
-        if LCapacity = 0 then LCapacity := 8
-        else LCapacity := LCapacity * 2;
-        SetLength(LList, LCapacity);
-      end;
-      LList[LCount] := ParseGoBenchLine(LLine);
-      Inc(LCount);
-    except
-      Inc(GLastParseSkippedCount);
-    end;
-  end;
-  SetLength(LList, LCount);
-  Result := LList;
+function GoLineMatcher(const ALine: string): Boolean;
+begin
+  Result := Pos('Benchmark', ALine) = 1;
+end;
+
+function RustLineMatcher(const ALine: string): Boolean;
+begin
+  Result := Pos('time:', ALine) > 0;
+end;
+
+function FPCLineMatcher(const ALine: string): Boolean;
+begin
+  Result := Pos('NsPerOp', ALine) > 0;
+end;
+
+function ParseGoBenchOutput(const AOutput: string): TBenchResultArray;
+begin
+  Result := ParseLines(AOutput, @ParseGoBenchLine, @GoLineMatcher);
 end;
 
 { ParseRustBenchLine }
@@ -423,38 +462,8 @@ end;
 { ParseRustBenchOutput }
 
 function ParseRustBenchOutput(const AOutput: string): TBenchResultArray;
-var
-  LLines: TStringArray;
-  LLine: string;
-  LList: array of TBenchResult;
-  LCount: Integer;
-  LCapacity: Integer;
 begin
-  GLastParseSkippedCount := 0;
-  LList := nil;
-  LCount := 0;
-  LCapacity := 0;
-  LLines := SplitLines(AOutput);
-  for LLine in LLines do
-  begin
-    if IsEmptyOrComment(LLine) then Continue;
-    if Pos('time:', LLine) = 0 then Continue;
-
-    try
-      if LCount >= LCapacity then
-      begin
-        if LCapacity = 0 then LCapacity := 8
-        else LCapacity := LCapacity * 2;
-        SetLength(LList, LCapacity);
-      end;
-      LList[LCount] := ParseRustBenchLine(LLine);
-      Inc(LCount);
-    except
-      Inc(GLastParseSkippedCount);
-    end;
-  end;
-  SetLength(LList, LCount);
-  Result := LList;
+  Result := ParseLines(AOutput, @ParseRustBenchLine, @RustLineMatcher);
 end;
 
 { ParseFPCBenchLine }
@@ -501,38 +510,8 @@ end;
 { ParseFPCBenchOutput }
 
 function ParseFPCBenchOutput(const AOutput: string): TBenchResultArray;
-var
-  LLines: TStringArray;
-  LLine: string;
-  LList: array of TBenchResult;
-  LCount: Integer;
-  LCapacity: Integer;
 begin
-  GLastParseSkippedCount := 0;
-  LList := nil;
-  LCount := 0;
-  LCapacity := 0;
-  LLines := SplitLines(AOutput);
-  for LLine in LLines do
-  begin
-    if IsEmptyOrComment(LLine) then Continue;
-    if Pos('NsPerOp', LLine) = 0 then Continue;
-
-    try
-      if LCount >= LCapacity then
-      begin
-        if LCapacity = 0 then LCapacity := 8
-        else LCapacity := LCapacity * 2;
-        SetLength(LList, LCapacity);
-      end;
-      LList[LCount] := ParseFPCBenchLine(LLine);
-      Inc(LCount);
-    except
-      Inc(GLastParseSkippedCount);
-    end;
-  end;
-  SetLength(LList, LCount);
-  Result := LList;
+  Result := ParseLines(AOutput, @ParseFPCBenchLine, @FPCLineMatcher);
 end;
 
 { ParseBenchOutput }
